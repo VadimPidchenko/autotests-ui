3321dbfdd50f153cade365a473ca2183c459a693
VadimPidchenko <pidchenko.vadim@icloud.com>
2026-06-05 14:17:53 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

---
+++ playwright_courses.py
@@ -0,0 +1,46 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id("registration-form-email-input").locator("input")
+    email_input.fill("my_user123@gmail.com")
+
+    username_input = page.get_by_test_id("registration-form-username-input").locator("input")
+    username_input.fill("my_user123")
+
+    password_input = page.get_by_test_id("registration-form-password-input").locator("input")
+    password_input.fill("12345")
+
+    registration_button = page.get_by_test_id("registration-page-registration-button")
+    registration_button.click()
+
+    context.storage_state(path="browser-state.json")
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    course_title = page.get_by_test_id("courses-list-toolbar-title-text")
+    expect(course_title).to_be_visible()
+    expect(course_title).to_have_text("Courses")
+
+    course_icon = page.get_by_test_id("courses-list-empty-view-icon")
+    expect(course_icon).to_be_visible()
+
+    course_title_results = page.get_by_test_id("courses-list-empty-view-title-text")
+    expect(course_title_results).to_be_visible()
+    expect(course_title_results).to_have_text("There is no results")
+
+    course_description_text = page.get_by_test_id("courses-list-empty-view-description-text")
+    expect(course_description_text).to_be_visible()
+    expect(course_description_text).to_have_text("Results from the load test pipeline will be displayed here")
+
+
